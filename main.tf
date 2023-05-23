703459bb95384baa9a777435184710ad699be6bf
Johnson Howard <jhoward@adaptavist.com>
2023-05-23 15:11:08 +0100

    Skip VPC checkiov

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ resource "aws_lambda_function" "this" {
   #checkov:skip=CKV_AWS_50:X-ray tracing not enforced in Adaptavist
   #checkov:skip=CKV_AWS_115:Lambda dead letter queue not enforced in Adaptavist
   #checkov:skip=CKV_AWS_116:Lambda dead letter queue not enforced in Adaptavist
+  #checkov:skip=CKV_AWS_117:Lambdas in the default VPC security groups are hardened appropriately
 
   function_name = local.function_name
   description   = var.description
